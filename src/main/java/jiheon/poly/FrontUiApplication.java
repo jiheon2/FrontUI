bf1f859ad7002b4ff793d41b80e9e5f694999be6
jiheon2 <000224kjh@gmail.com>
2024-04-02 09:04:26 +0900

    first commit

---
+++ src/main/java/jiheon/poly/FrontUiApplication.java
@@ -0,0 +1,13 @@
+package jiheon.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontUiApplication.class, args);
+    }
+
+}
